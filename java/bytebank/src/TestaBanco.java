c596ddd1ca7d20638c660d9bfadd30155578bfc8
jhonatan <podearroztkd@gmail.com>
2021-06-08 18:43:37 -0300

    arquivos substituidos

--- java/bytebank/src/TestaBanco.java
+++ java/bytebank/src/TestaBanco.java
@@ -1,9 +1,16 @@
-
 public class TestaBanco {
 	public static void main(String[] args) {
-		Conta contaDaMarcela = new Conta();
-		contaDaMarcela.titular = new Cliente();
-		
-		contaDaMarcela.titular.nome = "Marcela";
+		Cliente paulo = new Cliente();
+		paulo.setNome("Paulo Silveira");
+		paulo.setCpf("222.222.222.-22");
+		paulo.setProfissao("programador");
+
+		Conta contaDoPaulo = new Conta(6646,969060);
+		contaDoPaulo.deposita(100);
+
+		contaDoPaulo.setTitular(paulo);
+		System.out.println(contaDoPaulo.getTitular().getNome());
+		System.out.println(contaDoPaulo.getTitular());
+
 	}
 }
