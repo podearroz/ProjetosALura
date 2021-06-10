e16dda2b2a5f071ac515912306ee549c7d5b7c3c
jhonatan <podearroztkd@gmail.com>
2021-06-10 15:50:52 -0300

    adicionado interface Autenticavel

--- java/bytebank-herdado/src/Gerente.java
+++ java/bytebank-herdado/src/Gerente.java
@@ -1,11 +1,13 @@
 
-public class Gerente extends Funcionario{
+public class Gerente extends Funcionario implements Autenticavel{
 	private int senha;
 	
+	
+	@Override
 	public void setSenha(int senha) {
 		this.senha = senha;
 	}
-	
+	@Override
 	public boolean autentica(int senha) {
 	    if (this.senha == senha) {
 	        return true;
