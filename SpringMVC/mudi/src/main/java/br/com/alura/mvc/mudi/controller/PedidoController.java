5402d02660d06d7a3a4c1be9ad9a0817d77677d6
jhonatan <podearroztkd@gmail.com>
2021-06-14 19:19:48 -0300

    editando o HTML do formulario

---
+++ SpringMVC/mudi/src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
@@ -0,0 +1,23 @@
+package br.com.alura.mvc.mudi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.alura.mvc.mudi.dto.requisicaoNovoPedido;
+
+@Controller
+@RequestMapping("pedido")
+public class PedidoController {
+	
+	@GetMapping("formulario")
+	public String formulario() {
+		return "pedido/formulario";
+	}
+	
+	@PostMapping("novo")
+	public String novo(requisicaoNovoPedido requisicao) {
+		return "pedido/formulario";
+	}
+}
