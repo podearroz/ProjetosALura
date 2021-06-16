9bf30144843dd3caacbe2427fec5c54913e179f3
jhonatan <podearroztkd@gmail.com>
2021-06-16 16:31:42 -0300

    executando comandos SQL no java

---
+++ java/loja-virtual-repository/src/TestaConexao.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+
+import java.sql.SQLException;
+public class TestaConexao {
+
+	public static void main(String[] args) throws SQLException {
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection con = connectionFactory.recuperarConexao();
+	
+	System.out.println("Fechando conexao");
+	con.close();
+	}
+
+}
