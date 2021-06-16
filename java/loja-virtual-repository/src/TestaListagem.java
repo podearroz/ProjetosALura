b50beecc91f1e247ff7b0f12524cd29cff1b7b1e
jhonatan <podearroztkd@gmail.com>
2021-06-16 16:52:30 -0300

    evitando SQL injection

--- java/loja-virtual-repository/src/TestaListagem.java
+++ java/loja-virtual-repository/src/TestaListagem.java
@@ -1,8 +1,8 @@
 import java.sql.Connection;
-
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+
 
 public class TestaListagem {
 
@@ -12,8 +12,8 @@ public class TestaListagem {
 		Connection con = connectionFactory.recuperarConexao();
 		
 		
-		Statement stm =con.createStatement();
-		stm.execute("SELECT ID, NOME, DESCRICAO FROM PRODUTO");
+		PreparedStatement stm =con.prepareStatement("SELECT ID, NOME, DESCRICAO FROM PRODUTO");
+		stm.execute();
 		
 		ResultSet rst = stm.getResultSet();
 		
---
+++ java/loja-virtual-repository/src/TesteInsercaoComParametro.java
@@ -0,0 +1,31 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TesteInsercaoComParametro {
+
+	public static void main(String[] args) throws SQLException {
+		String nome = "mouse'";
+		String descricao = "Mouse sem fio ); delete from produto;";		
+		ConnectionFactory factory = new ConnectionFactory();
+		Connection connection = factory.recuperarConexao();
+		
+		PreparedStatement stm = 
+				connection.prepareStatement("INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)",Statement.RETURN_GENERATED_KEYS );
+		stm.setString(1, nome);
+		stm.setString(2, descricao);
+		
+		stm.execute();
+		
+		ResultSet rst = stm.getGeneratedKeys();
+		while(rst.next()) {
+			Integer id = rst.getInt(1);
+			System.out.println("O id criado foi: "+ id);
+		}
+	}
+
+	}
+
+
