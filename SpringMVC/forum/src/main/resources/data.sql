18f43c52e231a81fc95bbda7956131a1e77e10be
jhonatan <podearroztkd@gmail.com>
2021-07-06 15:26:12 -0300

    proteção com Spring Security+
    Aula 3 de Spring Boot API Rest: Segurança da API, cache e Monitoramento

--- SpringMVC/forum/src/main/resources/data.sql
+++ SpringMVC/forum/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$EitFyRrhZzbETFPGG/2cvuFyhUvlSLYFm9R9iZxGb5RwxbqvygJ/u');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
