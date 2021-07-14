ec8405a97ab15969f35e84871dc8f23a89500b25
jhonatan <podearroztkd@gmail.com>
2021-07-14 17:11:52 -0300

    Mais segurança
    Aula 01 SpringBoot e teste: Profiles, Testes e Deploy

--- Testes/1843-spring-parte3-testes-deploy-projeto-inicial/1843-spring-parte3-testes-deploy-projeto-inicial/src/main/resources/data.sql
+++ Testes/1843-spring-parte3-testes-deploy-projeto-inicial/1843-spring-parte3-testes-deploy-projeto-inicial/src/main/resources/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$sFKmbxbG4ryhwPNx/l3pgOJSt.fW1z6YcUnuE2X8APA/Z3NI/oSpq');
 INSERT INTO USUARIO(nome, email, senha) VALUES('Moderador', 'moderador@email.com', '$2a$10$sFKmbxbG4ryhwPNx/l3pgOJSt.fW1z6YcUnuE2X8APA/Z3NI/oSpq');
 
-INSERT INTO PERFIL(id, nome) VALUES(1, 'ROLE_ALUNO');
-INSERT INTO PERFIL(id, nome) VALUES(2, 'ROLE_MODERADOR');
+INSERT INTO PERFIL(id,nome) VALUES(1, 'ROLE_ALUNO');
+INSERT INTO PERFIL(id,nome) VALUES(2, 'ROLE_MODERADOR');
 
 INSERT INTO USUARIO_PERFIS(usuario_id, perfis_id) VALUES(1, 1);
 INSERT INTO USUARIO_PERFIS(usuario_id, perfis_id) VALUES(2, 2);
