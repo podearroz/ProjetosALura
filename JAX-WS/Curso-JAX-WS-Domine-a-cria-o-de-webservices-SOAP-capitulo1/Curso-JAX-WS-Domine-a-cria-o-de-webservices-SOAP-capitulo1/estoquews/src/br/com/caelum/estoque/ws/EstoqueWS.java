8771fb10798cee7d35b534a930668a0b42ef435e
jhonatan <podearroztkd@gmail.com>
2021-07-22 16:58:52 -0300

    Trabalhando com cabeçalhos
    Aula 03: JAX-WS: Domine a criação de webservices SOAP

--- JAX-WS/Curso-JAX-WS-Domine-a-cria-o-de-webservices-SOAP-capitulo1/Curso-JAX-WS-Domine-a-cria-o-de-webservices-SOAP-capitulo1/estoquews/src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ JAX-WS/Curso-JAX-WS-Domine-a-cria-o-de-webservices-SOAP-capitulo1/Curso-JAX-WS-Domine-a-cria-o-de-webservices-SOAP-capitulo1/estoquews/src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -11,7 +11,11 @@ import br.com.caelum.estoque.modelo.item.Filtro;
 import br.com.caelum.estoque.modelo.item.Filtros;
 import br.com.caelum.estoque.modelo.item.Item;
 import br.com.caelum.estoque.modelo.item.ItemDao;
+import br.com.caelum.estoque.modelo.item.ItemValidador;
 import br.com.caelum.estoque.modelo.item.ListaItens;
+import br.com.caelum.estoque.modelo.usuario.AutorizacaoException;
+import br.com.caelum.estoque.modelo.usuario.TokenDao;
+import br.com.caelum.estoque.modelo.usuario.TokenUsuario;
 
 @WebService
 public class EstoqueWS {
@@ -27,5 +31,20 @@ public class EstoqueWS {
         List<Filtro> lista = filtros.getLista();
         List<Item> itensResultado= dao.todosItens(lista);
         return new ListaItens(itensResultado);
+    }
+    
+    
+    @WebMethod(operationName="CadastrarItem") 
+    public Item cadastrarItem(
+    		@WebParam(name="tokenUsuario", header=true) TokenUsuario token,
+    		@WebParam(name="item") Item item) throws AutorizacaoException {
+        System.out.println("Cadastrando " + item + ", + token");
+        if(!new TokenDao().ehValido(token)) {
+            throw new AutorizacaoException("Autorizacao falhou");
+        }
+        
+        new ItemValidador(item).validate();
+        this.dao.cadastrar(item);
+        return item;
     }
 }
