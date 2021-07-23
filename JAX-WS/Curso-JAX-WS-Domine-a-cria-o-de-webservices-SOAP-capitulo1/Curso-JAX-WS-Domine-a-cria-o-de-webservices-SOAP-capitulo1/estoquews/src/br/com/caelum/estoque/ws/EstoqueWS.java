e0824cffbb027f4ec8d08fc7ffc3d76fc5b423f2
jhonatan <podearroztkd@gmail.com>
2021-07-23 15:38:59 -0300

    finalizando JAX-WS

--- JAX-WS/Curso-JAX-WS-Domine-a-cria-o-de-webservices-SOAP-capitulo1/Curso-JAX-WS-Domine-a-cria-o-de-webservices-SOAP-capitulo1/estoquews/src/br/com/caelum/estoque/ws/EstoqueWS.java
+++ JAX-WS/Curso-JAX-WS-Domine-a-cria-o-de-webservices-SOAP-capitulo1/Curso-JAX-WS-Domine-a-cria-o-de-webservices-SOAP-capitulo1/estoquews/src/br/com/caelum/estoque/ws/EstoqueWS.java
@@ -6,6 +6,9 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.ParameterStyle;
+import javax.jws.soap.SOAPBinding.Style;
 
 import br.com.caelum.estoque.modelo.item.Filtro;
 import br.com.caelum.estoque.modelo.item.Filtros;
@@ -18,6 +21,7 @@ import br.com.caelum.estoque.modelo.usuario.TokenDao;
 import br.com.caelum.estoque.modelo.usuario.TokenUsuario;
 
 @WebService
+@SOAPBinding(style=Style.RPC)
 public class EstoqueWS {
 
     private ItemDao dao = new ItemDao();
